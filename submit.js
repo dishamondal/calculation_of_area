3ac14f3d50fb08ff42274744b6c65baae681018a
Disha Mondal <133192704+dishamondal@users.noreply.github.com>
2023-06-21 16:02:11 +0530

    Add files via upload

---
+++ submit.js
@@ -0,0 +1,10 @@
+function calculateArea(length, width) {
+    var area = length * width;
+    return area;
+  }
+  
+var length = 5;
+var width = 10;
+
+var area = calculateArea(length, width);
+console.log("The area of the rectangle is: " + area);
